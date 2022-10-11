810ac2655e749dd8a341e6e467fe8e882f7c2051
Ardan <aulia.rahman71@ui.ac.id>
2022-10-12 00:05:10 +0700

    yes

--- assets/MainBrain.js
+++ assets/MainBrain.js
@@ -1,12 +1,54 @@
+const questions = [
+  "",
+  "",
+  "",
+  
+]
+
 const cardHandler = {
     displayQuestion: '',
     hasChoosen: false,
     gameStarted: false,
 };
 
-var cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
     card.classList.toggle('is-flipped');
   });
 });
+
+const cardHolder = document.querySelector('.card-holder');
+const btn_startGame = document.querySelector('#startButton');
+const txt_countDown = document.querySelector('.countdown');
+
+function startGame () {
+  let counter = 3;
+  btn_startGame.classList.toggle('turn-off');
+  const startInterval = setInterval(() =>{
+    if (counter == 0){
+      txt_countDown.innerHTML = "Let's play, choose your card!";
+      setTimeout(() =>{
+        txt_countDown.innerHTML = "";
+      }, 1000);
+      setTimeout(() =>{
+        cardHolder.classList.add('turn-on');
+        loadQuestions();
+      }, 3000);
+      clearInterval(startInterval);
+    } else{
+      txt_countDown.innerHTML = counter;
+      counter--;
+    }
+  }, 1000);
+}
+
+function loadQuestions (){
+  if(cardHandler.gameStarted == true)
+    return;
+  cardHandler.gameStarted = true;
+  
+}
+
+btn_startGame.addEventListener('click', startGame);
+
